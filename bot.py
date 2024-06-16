762e94052755d40cff2a1c67bb5289c9b79fc630
qf3l3k <36550920+qf3l3k@users.noreply.github.com>
2024-06-16 08:02:10 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,23 +4,23 @@ import os
 import discord
 from discord.ext import commands
 
-# Import config and logging functions
-from config_loader.config_manager import config
-from logger_setup.logger_config import setup_logging
-
-# Import setup functions from command and event modules
 from bot_commands.general_commands import setup as setup_general_commands
 from bot_events.general_events import setup as setup_general_events
+from config_loader.appconfig import AppConfig
+from logger_setup.logger_config import setup_logging
+
 
 bot_dir = os.path.abspath(os.path.dirname(__file__))
 
-setup_logging(f'{bot_dir}/log/discord-bot.log')
+setup_logging(f'{bot_dir}/log/furball.log')
 bot_logger = logging.getLogger()
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-setup_general_events(bot)
+config = AppConfig(bot_dir)
 setup_general_commands(bot)
+setup_general_events(bot, config)
+
 
 bot.run(config.discord_params['DISCORD_TOKEN'])
